7721e51c6198aba0f080aec4093b11a5d111570b
Michael Gallagher <mfgallagher@middlebury.edu>
2019-11-16 16:23:02 +0000

    Homepage has more or less the first componenets it needs, the log in path works but the sign up path does not for some reason, need to figure that out. Also need a clearer sense of what is happening/the route paths that I need to define/that I can just pull from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  attr_accessor :email, :password, :password_confirmation, :remember_me
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => {sign_in: "login", sign_out: "logout"} ,
+                     :controllers => {registrations: "registrations"}
+                    
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/mission'
   root 'static_pages#home'
+  resources :users
 end 
