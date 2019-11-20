90d356c57a0357b9065319c4b9d314587a536962
Michael Gallagher <mfgallagher@middlebury.edu>
2019-11-20 21:12:31 +0000

    Email authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :path_names => {sign_in: "login", sign_out: "logout"} 
                      
-                    
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/mission'
