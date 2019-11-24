4cb8c66724c286dd96029822415b7d6840a3eeaf
Michael Gallagher <mfgallagher@middlebury.edu>
2019-11-24 22:14:28 +0000

    I think I'm running into problems with the routing, but the initial codebase for the questions seems to be there

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :path_names => {sign_in: "login", sign_out: "logout"} 
+  resources :users, only: [:show]
                      
-  get 'static_pages/home'
+ 
   get 'static_pages/help'
   get 'static_pages/mission'
   get '/users/sign_up'
   get '/users/login'
+  get 'users/id' => 'users#show'
   root 'static_pages#home'
-  resources :users
+  
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_213835) do
+ActiveRecord::Schema.define(version: 2019_11_24_181852) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_questions_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,14 +27,17 @@ ActiveRecord::Schema.define(version: 2019_11_17_213835) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
