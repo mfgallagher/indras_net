8177b25a7d222f1fc62a35bdcde4b7f5a0477580
Justin Michael Doran <jmdoran@gmail.com>
2019-12-31 14:49:18 -0500

    Patched up doubly-nested resource logic; this is an ugly hack.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy, :index, :show]
   before_action :correct_user, only: :destroy
-  
+
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.build(answer_params)
@@ -9,7 +9,7 @@ class AnswersController < ApplicationController
     @answer.user= current_user
     if @answer.save
       flash[:success] = "Thanks for offering your input!"
-      redirect_to @question
+      redirect_to community_question_path(@answer.community, @question)
     else
       flash[:error] = "invalid answer"
       redirect_to root_url
@@ -17,16 +17,16 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @question.destroy
+    @answer.destroy
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || root_url
-  end 
-  
+  end
+
   def show
     @question = Question.find(params[:question_id])
     @answers = @question.answers
   end
-  
+
   def index
     @question = Question.find(params[:question_id])
     @answers = @question.answers
@@ -34,14 +34,14 @@ class AnswersController < ApplicationController
 
 
 private
-  
+
     def answer_params
       params.require(:answer).permit(:content)
     end
-    
+
     def correct_user
       @answer = current_user.answers.find_by(id: params[:id])
       redirect_to root_url if @answer.nil?
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users, :path_prefix => 'd',
-             :path_names => {sign_in: "login", sign_out: "logout"} 
-                     
+             :path_names => {sign_in: "login", sign_out: "logout"}
+
   get "static_pages/help", to: "static_pages#help", as: "help"
   get "static_pages/mission", to: "static_pages#mission", as: "mission"
   get "communities", to: "communities#index", as: "communities"
   get '/users/sign_up'
   get '/users/login'
   get "communities/:id/join", to: "community_users#new", as: "community_join"
-  post "communities/:id", to: "questions#create"
-  
+  #post "communities/:id", to: "questions#create"
+
   root 'static_pages#home'
-  resources :communities, only: [:create, :show, :index]
-  resources :users, only: [:show]
-  resources :questions, only: [:create, :destroy, :show, :index] do
-    resources :answers, only: [:create, :destroy, :show, :index]
+  resources :communities, only: [:create, :show, :index] do
+    resources :questions, only: [:create, :destroy, :show, :index] do
+      resources :answers, only: [:create, :destroy, :show, :index]
+    end
   end
-end 
+  resources :users, only: [:show]
+
+end
